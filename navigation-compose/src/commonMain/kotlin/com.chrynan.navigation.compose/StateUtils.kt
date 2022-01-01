3a0faabf98a58996aeac9b536d68cb215ae6648e
Christopher Keenan <ckeenan@starry.com>
2022-01-01 11:17:23 -0600

    Fixed build issues with default parameter values in the StateUtils functions

--- navigation-compose/src/commonMain/kotlin/com.chrynan.navigation.compose/StateUtils.kt
+++ navigation-compose/src/commonMain/kotlin/com.chrynan.navigation.compose/StateUtils.kt
@@ -22,8 +22,29 @@ import androidx.compose.runtime.collectAsState
  */
 @ExperimentalNavigationApi
 @Composable
-fun <Key> ComposeNavigator<Key>.currentKeyAsState(initialCurrentKey: Key = currentKey): State<Key> =
+fun <Key> ComposeNavigator<Key>.currentKeyAsState(initialCurrentKey: Key): State<Key> =
     keyChanges.collectAsState(initial = initialCurrentKey)
+
+/**
+ * Obtains the changes to the [ComposeNavigator.currentKey] value and returns it as a [State]. This allows it to be
+ * used in a [Composable] and cause recomposition when the value changes.
+ *
+ * If you just need to get the current key value and do not need to cause recomposition when the value changes, simply
+ * use the [ComposeNavigator.currentKey] property.
+ *
+ * **Note:** Internally this function uses the [ComposeNavigator.keyChanges] Flow and the [collectAsState] function
+ * using the [ComposeNavigator.currentKey] as the initial value.
+ *
+ * @see [ComposeNavigator.currentKey]
+ * @see [ComposeNavigator.keyChanges]
+ * @see [collectAsState]
+ */
+// **Note:** For some reason adding a default value to the `initialCurrentKey` parameter in the [currentKeyAsState]
+// function is causing an issue so this function is needed for the default case.
+@ExperimentalNavigationApi
+@Composable
+fun <Key> ComposeNavigator<Key>.currentKeyAsState(): State<Key> =
+    keyChanges.collectAsState(initial = currentKey)
 
 /**
  * Obtains the changes to the [ComposeScopedNavigator.currentScope] value and returns it as a [State]. This allows it
@@ -41,5 +62,26 @@ fun <Key> ComposeNavigator<Key>.currentKeyAsState(initialCurrentKey: Key = curre
  */
 @ExperimentalNavigationApi
 @Composable
-fun <Scope, Key> ComposeScopedNavigator<Scope, Key>.currentScopeAsState(initialCurrentScope: Scope = currentScope): State<Scope> =
+fun <Scope, Key> ComposeScopedNavigator<Scope, Key>.currentScopeAsState(initialCurrentScope: Scope): State<Scope> =
     scopeChanges.collectAsState(initial = initialCurrentScope)
+
+/**
+ * Obtains the changes to the [ComposeScopedNavigator.currentScope] value and returns it as a [State]. This allows it
+ * to be used in a [Composable] and cause recomposition when the value changes.
+ *
+ * If you just need to get the current scope value and do not need to cause recomposition when the value changes, simply
+ * use the [ComposeScopedNavigator.currentScope] property.
+ *
+ * **Note:** Internally this function uses the [ComposeScopedNavigator.scopeChanges] Flow and the [collectAsState]
+ * function using the [ComposeScopedNavigator.currentScope] as the initial value.
+ *
+ * @see [ComposeScopedNavigator.currentScope]
+ * @see [ComposeScopedNavigator.scopeChanges]
+ * @see [collectAsState]
+ */
+// **Note:** For some reason adding a default value to the `initialCurrentScope` parameter in the [currentScopeAsState]
+// function is causing an issue so this function is needed for the default case.
+@ExperimentalNavigationApi
+@Composable
+fun <Scope, Key> ComposeScopedNavigator<Scope, Key>.currentScopeAsState(): State<Scope> =
+    scopeChanges.collectAsState(initial = currentScope)
