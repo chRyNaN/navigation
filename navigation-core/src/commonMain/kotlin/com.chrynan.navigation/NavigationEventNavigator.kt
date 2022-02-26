413325f190d26c219f4f3397366e8dbf5f0f8f33
Christopher Keenan <ckeenan@starry.com>
2022-02-26 16:05:56 -0600

    Moved NavigationEventNavigator utils to NavigationEventNavigator file

--- navigation-core/src/commonMain/kotlin/com.chrynan.navigation/NavigationEventNavigator.kt
+++ navigation-core/src/commonMain/kotlin/com.chrynan.navigation/NavigationEventNavigator.kt
@@ -13,3 +13,21 @@ interface NavigationEventNavigator<I : NavigationIntent> : Navigator {
      */
     fun navigate(event: NavigationEvent<I>)
 }
+
+/**
+ * A convenience function for calling [NavigationEventNavigator.navigate] with [NavigationEvent.Back].
+ */
+fun <I : NavigationIntent> NavigationEventNavigator<I>.goBack() =
+    navigate(event = NavigationEvent.Back())
+
+/**
+ * A convenience function for calling [NavigationEventNavigator.navigate] with [NavigationEvent.Up].
+ */
+fun <I : NavigationIntent> NavigationEventNavigator<I>.goUp() =
+    navigate(event = NavigationEvent.Up())
+
+/**
+ * A convenience function for calling [NavigationEventNavigator.navigate] with [NavigationEvent.To].
+ */
+fun <I : NavigationIntent> NavigationEventNavigator<I>.goTo(intent: I) =
+    navigate(event = NavigationEvent.To(intent = intent))
