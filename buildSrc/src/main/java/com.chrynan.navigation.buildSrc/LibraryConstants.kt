452db3a124b4cf44757a0c22b841ed46409ae4eb
Christopher Keenan <ckeenan@starry.com>
2022-03-03 21:39:57 -0600

    Bumped version to 0.4.0 (6)

--- buildSrc/src/main/java/com.chrynan.navigation.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.navigation.buildSrc/LibraryConstants.kt
@@ -7,8 +7,8 @@ object LibraryConstants {
     const val group = "com.chrynan.navigation"
     const val owner = "chrynan"
     const val repoName = "navigation"
-    const val versionName = "0.3.0"
-    const val versionCode = 5
+    const val versionName = "0.4.0"
+    const val versionCode = 6
     const val versionDescription = "Release $versionName ($versionCode)"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/navigation.git"
