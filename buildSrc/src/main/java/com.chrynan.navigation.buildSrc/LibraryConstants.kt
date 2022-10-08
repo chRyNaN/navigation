ed1db839b38af386929823d161d92942f42a48ac
Chris <ckeenan@starry.com>
2022-10-08 10:02:37 -0500

    Bumped version to 0.6.0 (8)

--- buildSrc/src/main/java/com.chrynan.navigation.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.navigation.buildSrc/LibraryConstants.kt
@@ -7,8 +7,8 @@ object LibraryConstants {
     const val group = "com.chrynan.navigation"
     const val owner = "chrynan"
     const val repoName = "navigation"
-    const val versionName = "0.5.0"
-    const val versionCode = 7
+    const val versionName = "0.6.0"
+    const val versionCode = 8
     const val versionDescription = "Release $versionName ($versionCode)"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/navigation.git"
