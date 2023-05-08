d5353b1ad431b1f4749fa95ef7b1364c6c09a14f
Chris Keenan <chrynan@pm.me>
2023-05-08 14:46:55 -0500

    Removed NavigationScope

--- navigation-core/src/commonMain/kotlin/com.chrynan.navigation/NavigationScope.kt
+++
@@ -1,9 +0,0 @@
-package com.chrynan.navigation
-
-/**
- * Provides the ability for extra platform specific parameters to be provided to a [Navigator].
- */
-interface NavigationScope {
-
-    companion object
-}
