49710b831806fdf0ac30413d556bce3fe96d07ff
Chris Keenan <chrynan@pm.me>
2023-05-08 12:11:16 -0500

    Implemented MapBasedMutableNavigationStateStore

--- navigation-core/src/commonMain/kotlin/com.chrynan.navigation/NavigationEvent.kt
+++ navigation-core/src/commonMain/kotlin/com.chrynan.navigation/NavigationEvent.kt
@@ -11,12 +11,12 @@ import kotlinx.serialization.Serializable
  * Represents a navigation event that is sent to a [Navigator] to coordinate the navigation between UI components, such
  * as "screens" within an application. A [NavigationEvent] can be a [NavigationEvent.Destination] representing a change
  * in a [NavigationDestination] in the current context, a [NavigationEvent.Context] representing a change in
- * [NavigationContext], or a [NavigationEvent.Back] representing a back tracking of a previous [NavigationEvent].
+ * [NavigationContext], or a [NavigationEvent.Backward] representing a back tracking of a previous [NavigationEvent].
  *
  * @see [Navigator.navigate]
  */
 @Serializable
-sealed class NavigationEvent<D : NavigationDestination, C : NavigationContext<D>> private constructor() {
+sealed class NavigationEvent<D : NavigationDestination, C : NavigationContext<D>> {
 
     /**
      * The [Instant] that the event occurred.
@@ -24,7 +24,33 @@ sealed class NavigationEvent<D : NavigationDestination, C : NavigationContext<D>
     abstract val instant: Instant
 
     /**
-     * A [NavigationEvent] that represents a reversal of a previous navigation event. A [Back] navigation event can be
+     * The navigation direction for this event.
+     */
+    abstract val direction: Direction
+
+    /**
+     * Represents a direction for a [NavigationEvent]. A [NavigationEvent] can either be a [FORWARDS] direction event,
+     * meaning the change is added to a [Stack], or a [BACKWARDS] direction event, meaning the change causes a removal
+     * from a [Stack].
+     */
+    @Serializable
+    enum class Direction(val serialName: String) {
+
+        /**
+         * The associated [NavigationEvent] causes a removal of a previous event change from a [Stack].
+         */
+        @SerialName(value = "backwards")
+        BACKWARDS(serialName = "backwards"),
+
+        /**
+         * The associated [NavigationEvent] causes an addition of the change to a [Stack].
+         */
+        @SerialName(value = "forwards")
+        FORWARDS(serialName = "forwards")
+    }
+
+    /**
+     * A [NavigationEvent] that represents a reversal of a previous navigation event. A [Backward] navigation event can be
      * used to go to the previous [NavigationDestination] within the current [NavigationContext], or going back to a
      * previous [NavigationContext] before a change in context. The type of back navigation that is supported is
      * defined by the provided [kind] property.
@@ -35,14 +61,16 @@ sealed class NavigationEvent<D : NavigationDestination, C : NavigationContext<D>
      */
     @Serializable
     @SerialName(value = "back")
-    class Back<D : NavigationDestination, C : NavigationContext<D>> internal constructor(
+    class Backward<D : NavigationDestination, C : NavigationContext<D>> internal constructor(
         @SerialName(value = "instant") override val instant: Instant = Clock.System.now(),
         @SerialName(value = "kind") val kind: Kind
     ) : NavigationEvent<D, C>() {
 
+        override val direction: Direction = Direction.BACKWARDS
+
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
-            if (other !is Back<*, *>) return false
+            if (other !is Backward<*, *>) return false
 
             if (instant != other.instant) return false
 
@@ -56,7 +84,7 @@ sealed class NavigationEvent<D : NavigationDestination, C : NavigationContext<D>
         }
 
         override fun toString(): String =
-            "NavigationEvent.Back(instant=$instant, type=$kind)"
+            "NavigationEvent.Backward(instant=$instant, type=$kind, direction=$direction)"
 
         /**
          * Represents the type of supported back navigation. An [IN_CONTEXT] value indicates that navigation to the
@@ -64,7 +92,7 @@ sealed class NavigationEvent<D : NavigationDestination, C : NavigationContext<D>
          * indicates that navigation across [NavigationContext]s is allowed, meaning that navigation can either be to
          * the previous [NavigationDestination] within the current [NavigationContext] or to the previous
          * [NavigationContext] depending on whether the previous [NavigationEvent] was a [NavigationEvent.Destination]
-         * or [NavigationEvent.Context] event.
+         * or [NavigationEvent.Forward.Context] event.
          */
         @Serializable
         enum class Kind(val serialName: String) {
@@ -80,7 +108,7 @@ sealed class NavigationEvent<D : NavigationDestination, C : NavigationContext<D>
              * Indicates that navigation across [NavigationContext]s is allowed, meaning that navigation can either be
              * to the previous [NavigationDestination] within the current [NavigationContext] or to the previous
              * [NavigationContext] depending on whether the previous [NavigationEvent] was a
-             * [NavigationEvent.Destination] or [NavigationEvent.Context] event
+             * [NavigationEvent.Forward.Destination] or [NavigationEvent.Forward.Context] event
              */
             @SerialName(value = "across_context")
             ACROSS_CONTEXTS(serialName = "across_context")
@@ -88,68 +116,81 @@ sealed class NavigationEvent<D : NavigationDestination, C : NavigationContext<D>
     }
 
     /**
-     * A [NavigationEvent] that changes the [NavigationDestination] within the current [NavigationContext].
-     *
-     * @property [instant] The [Instant] that the event occurred.
-     * @property [destination] The [NavigationDestination] to go to. This value will be added to the top of the [Stack]
-     * of destinations within the current [NavigationContext].
+     * A [NavigationEvent] that represents a forward movement, or addition of a navigation change. These events can be
+     * reversed by a [Backward] event. There are two forward navigation events: [Destination] representing a change in
+     * destination on the current context stack, and [Context] representing a change in the context.
      */
     @Serializable
-    @SerialName(value = "destination")
-    class Destination<D : NavigationDestination, C : NavigationContext<D>> internal constructor(
-        @SerialName(value = "instant") override val instant: Instant = Clock.System.now(),
-        @SerialName(value = "destination") val destination: D
-    ) : NavigationEvent<D, C>() {
+    @SerialName(value = "forward")
+    sealed class Forward<D : NavigationDestination, C : NavigationContext<D>> private constructor() :
+        NavigationEvent<D, C>() {
 
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (other !is Destination<*, *>) return false
+        override val direction: Direction = Direction.FORWARDS
 
-            if (instant != other.instant) return false
+        /**
+         * A [NavigationEvent] that changes the [NavigationDestination] within the current [NavigationContext].
+         *
+         * @property [instant] The [Instant] that the event occurred.
+         * @property [destination] The [NavigationDestination] to go to. This value will be added to the top of the [Stack]
+         * of destinations within the current [NavigationContext].
+         */
+        @Serializable
+        @SerialName(value = "destination")
+        class Destination<D : NavigationDestination, C : NavigationContext<D>> internal constructor(
+            @SerialName(value = "instant") override val instant: Instant = Clock.System.now(),
+            @SerialName(value = "destination") val destination: D
+        ) : Forward<D, C>() {
 
-            return destination == other.destination
+            override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (other !is Destination<*, *>) return false
+
+                if (instant != other.instant) return false
+
+                return destination == other.destination
+            }
+
+            override fun hashCode(): Int {
+                var result = instant.hashCode()
+                result = 31 * result + destination.hashCode()
+                return result
+            }
+
+            override fun toString(): String =
+                "NavigationEvent.Forward.Destination(instant=$instant, destination=$destination, direction=$direction)"
         }
 
-        override fun hashCode(): Int {
-            var result = instant.hashCode()
-            result = 31 * result + destination.hashCode()
-            return result
+        /**
+         * A [NavigationEvent] that changes the current [NavigationContext].
+         *
+         * @property [instant] The [Instant] that the event occurred.
+         * @property [context] The [NavigationContext] to go to.
+         */
+        @Serializable
+        @SerialName(value = "context")
+        class Context<D : NavigationDestination, C : NavigationContext<D>> internal constructor(
+            @SerialName(value = "instant") override val instant: Instant = Clock.System.now(),
+            @SerialName(value = "context") val context: C
+        ) : Forward<D, C>() {
+
+            override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (other !is Context<*, *>) return false
+
+                if (instant != other.instant) return false
+
+                return context == other.context
+            }
+
+            override fun hashCode(): Int {
+                var result = instant.hashCode()
+                result = 31 * result + context.hashCode()
+                return result
+            }
+
+            override fun toString(): String =
+                "NavigationEvent.Forward.Context(instant=$instant, context=$context, direction=$direction)"
         }
-
-        override fun toString(): String =
-            "NavigationEvent.Destination(instant=$instant, destination=$destination)"
-    }
-
-    /**
-     * A [NavigationEvent] that changes the current [NavigationContext].
-     *
-     * @property [instant] The [Instant] that the event occurred.
-     * @property [context] The [NavigationContext] to go to.
-     */
-    @Serializable
-    @SerialName(value = "context")
-    class Context<D : NavigationDestination, C : NavigationContext<D>> internal constructor(
-        @SerialName(value = "instant") override val instant: Instant = Clock.System.now(),
-        @SerialName(value = "context") val context: C
-    ) : NavigationEvent<D, C>() {
-
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (other !is Context<*, *>) return false
-
-            if (instant != other.instant) return false
-
-            return context == other.context
-        }
-
-        override fun hashCode(): Int {
-            var result = instant.hashCode()
-            result = 31 * result + context.hashCode()
-            return result
-        }
-
-        override fun toString(): String =
-            "NavigationEvent.Context(instant=$instant, context=$context)"
     }
 
     companion object
--- navigation-core/src/commonMain/kotlin/com.chrynan.navigation/NavigationStateStore.kt
+++ navigation-core/src/commonMain/kotlin/com.chrynan.navigation/NavigationStateStore.kt
@@ -31,8 +31,12 @@ internal interface MutableNavigationStateStore<Destination : NavigationDestinati
 
     /**
      * Dispatches the provided navigation [event] which mutates the underlying state values.
+     *
+     * @return `true` if the navigation event was handled, or `false` if the event could not be handled (ex: a back
+     * navigation event was provided but there are no destinations to go back to). If `false` is returned, the
+     * underlying state values were not mutated.
      */
-    fun dispatch(event: NavigationEvent<Destination, Context>)
+    fun dispatch(event: NavigationEvent<Destination, Context>): Boolean
 
     /**
      * Resets the underlying state values back to their initial values.
@@ -53,27 +57,148 @@ internal fun <Destination : NavigationDestination, Context : NavigationContext<D
  * [NavigationDestination] [Stack]s in an in-memory [Map].
  */
 internal class MapBasedMutableNavigationStateStore<Destination : NavigationDestination, Context : NavigationContext<Destination>> internal constructor(
-    initialContext: Context
+    private val initialContext: Context
 ) : MutableNavigationStateStore<Destination, Context> {
 
     override val event = mutableNavigationStateOf<NavigationEvent<Destination, Context>?>(initial = null)
     override val destination = mutableNavigationStateOf(initial = initialContext.initialDestination)
     override val context = mutableNavigationStateOf(initial = initialContext)
 
-    private val map = mutableMapOf(initialContext to mutableStackOf(initialContext.initialDestination))
+    private val destinationStacksByContext =
+        mutableMapOf(initialContext to mutableStackOf(initialContext.initialDestination))
+    private val forwardNavigationEventStack = mutableStackOf<NavigationEvent.Forward<Destination, Context>>()
 
-    override fun dispatch(event: NavigationEvent<Destination, Context>) {
-        this.event.update(event)
+    override fun dispatch(event: NavigationEvent<Destination, Context>): Boolean =
+        when (event) {
+            is NavigationEvent.Backward -> goBack(event = event)
 
-        TODO("Not yet implemented")
-    }
+            is NavigationEvent.Forward.Destination -> {
+                forwardNavigationEventStack.push(event)
+
+                val context = peekCurrentContext()
+
+                pushDestinationToContextStack(context = context, destination = event.destination)
+
+                updateState(
+                    event = event,
+                    context = context,
+                    destination = event.destination
+                )
+
+                true
+            }
+
+            is NavigationEvent.Forward.Context -> {
+                forwardNavigationEventStack.push(event)
+
+                updateState(
+                    event = event,
+                    context = event.context,
+                    destination = peekCurrentDestinationForContext(context = event.context)
+                )
+
+                true
+            }
+        }
 
     override fun reset() {
         event.reset()
         destination.reset()
         context.reset()
 
-        map.clear()
-        map[context.initial] = mutableStackOf(context.initial.initialDestination)
+        destinationStacksByContext.clear()
+        destinationStacksByContext[context.initial] = mutableStackOf(context.initial.initialDestination)
+    }
+
+    private fun goBack(event: NavigationEvent.Backward<Destination, Context>): Boolean {
+        if (forwardNavigationEventStack.isEmpty()) return false
+
+        when (val currentEvent = forwardNavigationEventStack.pop()) {
+            is NavigationEvent.Forward.Context -> {
+                // If we can't navigate back across contexts, return false
+                if (event.kind != NavigationEvent.Backward.Kind.ACROSS_CONTEXTS) {
+                    // Add the current event back on to the stack before we return false, because we can't perform the
+                    // action, so we don't want to change the stack from when we started.
+                    forwardNavigationEventStack.push(currentEvent)
+
+                    return false
+                }
+
+                // Get the current context after we popped the last context change from the top of the stack.
+                val newContext = peekCurrentContext()
+
+                updateState(
+                    event = event,
+                    context = newContext,
+                    destination = peekCurrentDestinationForContext(context = newContext)
+                )
+
+                return true
+            }
+
+            is NavigationEvent.Forward.Destination -> {
+                val context = peekCurrentContext()
+                val destination = popToPreviousDestinationForContext(context = context) ?: return false
+
+                updateState(
+                    event = event,
+                    context = context,
+                    destination = destination
+                )
+
+                return true
+            }
+        }
+    }
+
+    private fun updateState(
+        event: NavigationEvent<Destination, Context>,
+        context: Context,
+        destination: Destination
+    ) {
+        this.context.update(state = context)
+        this.destination.update(state = destination)
+        this.event.update(state = event)
+    }
+
+    private fun peekCurrentContext(): Context =
+        forwardNavigationEventStack
+            .filterIsInstance<NavigationEvent.Forward.Context<Destination, Context>>()
+            .lastOrNull()?.context ?: initialContext
+
+    private fun peekCurrentDestinationForContext(context: Context): Destination {
+        val destination = destinationStacksByContext[context]?.peekOrNull()
+
+        return if (destination == null) {
+            destinationStacksByContext[context] = mutableStackOf(context.initialDestination)
+
+            context.initialDestination
+        } else {
+            destination
+        }
+    }
+
+    /**
+     * Pops the top destination off the provided [context] stack and returns the new top destination, or `null` if the
+     * provided [context] stack could not be popped (there must be at least one item in the stack at all times).
+     */
+    private fun popToPreviousDestinationForContext(context: Context): Destination? {
+        val stack = destinationStacksByContext[context] ?: return null
+
+        if (stack.size <= 1) return null
+
+        stack.pop()
+
+        destinationStacksByContext[context] = stack
+
+        return stack.peek()
+    }
+
+    private fun pushDestinationToContextStack(context: Context, destination: Destination) {
+        val stack = destinationStacksByContext[context] ?: mutableStackOf(context.initialDestination)
+
+        stack.push(destination)
+
+        destinationStacksByContext[context] = stack
     }
 }
